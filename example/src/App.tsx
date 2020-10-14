eb8caf1ab9013885d717fa3c724c5154d3e521cc
Harrison Lo <harrison0723@gmail.com>
2020-10-14 16:52:54 +0800

    [example] page skeleton and datasets

---
+++ example/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Corner from './Corner'
+import Header from './Header'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='container'>
+      <Corner />
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ example/src/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import cover from './cover.svg'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <img src={cover} alt='cover' />
+    </div>
+  )
+}
+
+export default Header
